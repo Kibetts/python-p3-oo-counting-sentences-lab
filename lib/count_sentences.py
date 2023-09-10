75a00e57f150ba3bdf87828a7334df91db84d29c
Brian Kibet <kibetsbrian@gmail.com>
2023-09-10 15:00:46 +0300

    completed assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value):
+        self.value = value
+
+    def is_sentence(self):
+        return self.value.endswith(".")
+
+    def is_question(self):
+        return self.value.endswith("?")
+
+    def is_exclamation(self):
+        return self.value.endswith("!")
+
+    def count_sentences(self):
+        sentences = self.value.split('.')
+        return len(sentences)
+
